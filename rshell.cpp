5d664c0023063f3fa0192430a28a40e25f671fc9
Jenny Nguyen <jnguy138@hammer.cs.ucr.edu>
2016-02-05 13:13:11 -0800

    editted algorithm compilable

--- rshell.cpp
+++ rshell.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <stdlib.h>
 #include "shell.h"
 #include "parse.h"
 using namespace std;
 
 int main()
 {
-	return 0;
+	Shell sh;			//initialize shell object
+	bool status;			//check to cont. shell or not
+
+	sh.initialize();		//start shell program
+	status = sh.getStatus();
+	
+	if(status == true)
+	{
+		//cont. looping shell program
+	}
+	else 
+	{ return 0; }	//end and exit program	
+
 }
--- shell.h
+++ shell.h
@@ -9,12 +9,39 @@ using namespace std;
 class Shell
 {
 	private:
-		bool status;	//check status of whether shell cont. running or not
+		bool status;		//check status of whether shell cont. running or not
 	
 	public:
 		Shell(){};		//constructor
-		bool finish();	//check if shell is done
-		bool start();	//start shell
+		bool getStatus()
+		{ return status; }
+
+		void start()		//start shell program
+		{ status = true; }
+
+		void finish()		//shell is done
+		{ status = false; }
+		
+		void initialize()	//start shell
+		{
+			start();
+
+			while(status == true)
+			{
+				//user input and parse commands
+				
+				string input; 
+	
+				if(input != "exit")
+				{
+					//execute each parsed command
+				}
+				else
+				{ break; }	//if user input is exit then break loop and exit
+			}
+			
+			finish();
+		}
 };
 
 
