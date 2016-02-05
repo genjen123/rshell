c90341bdb7302b6553ee1c5d5d22e6dcef19837f
qlyu001 <qlyu001@ucr.edu>
2016-02-04 22:07:07 -0800

    nelly update

--- parse.h
+++ parse.h
@@ -1,21 +1,40 @@
 #ifndef PARSE_H
 #define PARSE_H
+#include<iostream>
+#include<stdlib.h>
+#include<istream>
 
-#include <vector>
-#include <string>
 using namespace std;
 
 class Parse
 {
 	private:
-		string before;		//temp variables
-		string after;	
-		vector<string> split(string str_input, string pattern);	//vector of split strings
+		//string before;		//temp variables
+		//string after;
+		//vector<string> split(string str_input, string pattern);	//vector of split strings
+    char line[256];
 
 	public:
 		Parse(){}; 	//constructor
-		string ParseSemi(string);	//parses by semi colon and #
-		string ParseCmd(string);	//parses by commands (&&, ||, etc.)
+        void readLine()
+        {
+
+            
+            char line[286];
+            cin.getline(line,286);
+                
+                
+            for(int i = 0; i < 3; ++i)
+            {
+                cout << line[i];
+            }
+                
+            return;
+                
+                
+        }
+		//string ParseSemi(string);	//parses by semi colon and #
+		//string ParseCmd(string);	//parses by commands (&&, ||, etc.)
 };
 
 #endif
