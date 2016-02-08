fa7e8deba52494b64e6fcc9eb8878c0276ba17da
Jenny Trang Nguyen <Jenny@MacBook-Pro.att.net>
2016-02-07 16:57:19 -0800

    parsed # and ; compilable

--- parse.h
+++ parse.h
@@ -3,19 +3,68 @@
 
 #include <vector>
 #include <string>
+#include <cstring>
 using namespace std;
 
 class Parse
 {
 	private:
-		string before;		//temp variables
-		string after;	
-		vector<string> split(string str_input, string pattern);	//vector of split strings
+		string str;		//temp variables
+		vector<string> strParts;	
+		//vector<string> splitStr(string input, string pattern);	//vector of split strings
 
 	public:
-		Parse(){}; 	//constructor
-		string ParseSemi(string);	//parses by semi colon and #
-		string ParseCmd(string);	//parses by commands (&&, ||, etc.)
+		//default constructors
+		Parse(){}; 	
+		Parse(string input): str(input){}
+
+		void parseAll(string input)
+		{
+			input = parseTag(input);
+			parseSemi(input);
+		}
+
+		string parseTag(string input)	//parses by semi colon and #
+		{
+			string strEdit;
+			int posTag = input.find("#");		//find position of #
+
+			if(posTag != string::npos)			//if found
+			{
+				strEdit = input.substr(0, posTag);	//cut string to #
+				//cout << strEdit << endl;
+			}
+			return strEdit;
+		}
+
+		void parseSemi(string input)
+		{
+			int posSemi = input.find(";");	//find position of ;
+			string strEdit;
+
+			while(posSemi != string::npos)
+			{
+				strEdit = input.substr(0, posSemi);		//cut str to ;
+				
+				if(strEdit != " ")
+				{
+					strParts.push_back(strEdit);		//push part to vector
+				}
+
+				//check for spacing later
+
+				input = input.substr(posSemi + 2);		//edit input string
+				posSemi = input.find(";");				//find other ; repeats
+			}	
+			/*
+			for(int i = 0; i < strParts.size(); i++)
+			{
+				cout << strParts.at(i) << endl;
+			}
+			*/
+		}
+
+		string ParseCmd(string input);	//parses by commands (&&, ||, etc.)
 };
 
 #endif
