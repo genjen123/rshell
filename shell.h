ce049f1eed5d1e5544b474be9f519381b179ab1a
Jenny <jnguy138@ucr.edu>
2016-02-10 11:36:35 -0800

    add parsing for commands and connectors and extra credit

--- shell.h
+++ shell.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unistd.h>
+#include <stdio.h>
 #include "parse.h"
 #include "commandnode.h"
 using namespace std;
@@ -24,15 +26,34 @@ class Shell
 		void finish()		//shell is done
 		{ status = false; }
 		
+		bool execute(const CommandNode &cn)
+		{
+			bool returnStatus = false;
+			cout << cn << endl;
+			return returnStatus;
+		}
+
 		void initialize()	//start shell
 		{
 			start();
 
-			while(status == true)	//user input and parse commands
+			while(status)	//user input and parse commands
 			{
 				//initializing variables
 				string input, command;
 				Parse str;				
+		
+				//generate username (extra credit)
+				char username[1024] = {0};
+				int getUsername_resp = getlogin_r(username, sizeof(username) - 1);
+
+				char hostName[1024] = {0};
+				int getHostName_resp = gethostname(hostName, sizeof(hostName) - 1);
+
+				if((getHostName_resp == 0) && (getUsername_resp == 0))
+				{ printf("%s@%s", username, hostName); }
+
+				cout << "$ ";
 
 				getline(cin, input);	//get line input
 				//cout << input << endl;
@@ -42,15 +63,16 @@ class Shell
 					string parseTag = str.split(input, "#").at(0);	//parse #
 
 					vector<string> parseCom = str.split(parseTag, ";");	//parse ;
-
-					//start parsing commands and connectors
+					
+					//go through command parts and create list of commandNodes
+					vector<CommandNode> commandList;
 					for(unsigned int i = 0; i < parseCom.size(); i++)
 					{
 						command = parseCom.at(i);	//separate commands
 
 						//initializing variables
 						string temp = "";
-						vector<string> parseCon; 
+						string connector = "";
 
 						//setting positions for || and &&
 						int orCon = command.find("||");
@@ -64,28 +86,56 @@ class Shell
 							if((orCon != string::npos) && (andCon != string::npos))		//if both are found
 							{
 								//orCon < andCon then first = orCon else first = andCon
-								first = (orCon < andCon) ? orCon : andCon;
+								if(orCon < andCon)
+								{
+									first = orCon;
+									connector = "||";
+								}
+								else if(orCon > andCon)
+								{
+									first = andCon;
+									connector = "&&";
+								}
 							}
-							else if((orCon != string::npos) && andCon == string::npos)	//if only || is found
-							{ first = orCon; }
+							else if((orCon != string::npos) && (andCon == string::npos))	//if only || is found
+							{ 
+								first = orCon; 
+								connector = "||";
+							}
 							else if((orCon == string::npos) && (andCon != string::npos))	//if only && is found
-							{ first = andCon; }
+							{ 
+								first = andCon; 
+								connector = "&&";
+							}
 
 							temp = command.substr(0, first);	//get parsed string
-							parseCon.push_back(temp);
-							command = command.substr(first + 2);
+							commandList.push_back(CommandNode(temp, connector));
+							command = command.substr(first + connector.length());
 							orCon = command.find("||");			//find repeat ||
 							andCon = command.find("&&");		//find repeat &&
 						}
 
 						if(command.length() > 0)				//if string isn't empty
-						{ parseCon.push_back(command); }
+						{ commandList.push_back(CommandNode(command, ";")); }
 
-						for(int j = 0; j < parseCon.size(); j++)
+						//go through each command and execute them
+						bool prevReturnStat = false;
+						bool currentReturnStat = false;
+
+						for(int j = 0; j < commandList.size(); j++)
 						{
-							cout << i + 1 << ") [" << j + 1 << "/" << parseCon.size()
-								 << "]" << str.leftTrim(parseCon.at(j), " ") << endl;
-						}
+							//cout << i + 1 << ") [" << j + 1 << "/" << parseCon.size()
+							//	 << "]" << str.leftTrim(parseCon.at(j), " ") << endl;
+							
+							currentReturnStat = execute(commandList.at(i));
+							prevReturnStat = currentReturnStat;
+							
+							if(!prevReturnStat)
+							{
+								//do something here
+							}
+							
+						}	
 					}
 				}
 				else
@@ -100,3 +150,4 @@ class Shell
 
 
 #endif
+
