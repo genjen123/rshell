14ac645ab641b2969433f3133227317202c0cbc4
Jenny Trang Nguyen <Jenny@MacBook-Pro.local>
2016-02-12 21:10:53 -0800

    add bool functions compilable

--- src/connector.h
+++ src/connector.h
@@ -1,8 +1,10 @@
 #ifndef CONNECTOR_H
 #define CONNECTOR_H
 
+//std included
 #include <iostream>
 #include <string>
+//files included
 #include "parse.h"
 using namespace std;
 
@@ -10,17 +12,29 @@ class Connector
 {
 	private:
 		string connector_type;	//gets connector type (&&, ||, etc.)
-	
+
 	public:
 		//default constructors
-		Connector(){};			
+		Connector()
+		{ connector_type = ""; }
+
 		Connector(const string &connector)
 		{ connector_type = connector; }
-
-		ostream& operator <<(ostream& os, const Connector c);
-
-		string getType() const		//returns type of the connector
-		{ return connector_type; }
+		
+		//get connector type
+		string getType() const;
+		
+		//bool functions
+		bool isOR() const 
+		{ return (connector_type == "||")?true:false; }
+		
+		bool isAND() const 
+		{ return (connector_type == "&&")?true:false; }
+		
+		bool isSEMI() const 
+		{ return (connector_type == ";")?true:false; }
+		
+		friend ostream& operator <<(ostream& os, const Connector c);
 };
 
 ostream& operator <<(ostream& os, const Connector c)
@@ -29,6 +43,7 @@ ostream& operator <<(ostream& os, const Connector c)
 	return os;
 }
 
-
+string Connector::getType() const	//returns type of the connector
+{ return connector_type; }
 
 #endif
