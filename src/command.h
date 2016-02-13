e531ffe543c98030b9326b791b46e3161da92928
Jenny Trang Nguyen <Jenny@MacBook-Pro.local>
2016-02-12 21:01:21 -0800

    finalized compilable

--- src/command.h
+++ src/command.h
@@ -1,79 +1,99 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include <string>
 #include <iostream>
+#include <string>
 #include <vector>
+
 #include "parse.h"
 using namespace std;
 
 class Command
 {
 	private:
-		vector<string> argList;	//argument list
-		string executable;	//executable of the command (ls, echo, etc.)
+		vector<string> argList;		//argument list
+		string executable;			//executable of the command (ls, echo, etc.)
 	
 	public:
-		//default constructor
-		Command(){};
+		Command(){};				//default constructor
+
 		Command(string command)
 		{
-			Parse p;		//parse object
-			
-			command = p.ltrim(command, " ");
-			vector<string> tmp = p.split(command, " ");		//parse found executable into vector
-			executable = p.ltrim(tmp.at(0), " ");	
+			Parse p;
+			command = p.ltrim(command," ");
+			vector<string> tmp = p.split(command," ");	//parse found executable into vector
+			executable = p.ltrim(tmp[0]," ");
+			//cout << executable << endl;
 
-			if(tmp.size() > 1)		
+			if(tmp.size() > 1)		//if vector > 1 add strings to argList vector
 			{
 				for(unsigned int i = 1; i < tmp.size(); i++)
 				{
-					string argVal = tmp.at(i);
-					argList.push_back(p.ltrim(argVal, " "));
-				}
+					string argVal = tmp[i];
+					argList.push_back(p.ltrim(argVal," "));
+				}	
 			}
 		}
 
-		string getExec() const					//get executable
+		//get executable
+		string getExec() const	
 		{ return executable; }
 
-		vector<string> getArgList() const		//get argList
-		{ return argList; }
+		//get c_str of executable
+		char* getExec_c_str() const
+		{ return const_cast<char*>(executable.c_str()); }
 
-		string getArgListStr() const			//get string of argList vector
+		//get argList
+		vector<string> getArgList()
+		{ return argList; }	
+		
+		//get ArgList strings
+		string getArgListStr() const
 		{
-			string newStr = "";
+			string newString = "";
 
 			for(unsigned int i = 0; i < argList.size(); i++)
 			{
-				if(i != 0)
-				{ newStr += " "; }				//increment spaces
-				
-				newStr += argList.at(i);
+				if( i != 0)			//add spaces cept for beginning
+				{ newString += " "; }
+
+				newString += argList[i];
 			}
-			return newStr;
+			return newString;
+		}
+
+		//get c_str of ArgList (same as previous function)
+		char* getArgList_c_str()
+		{
+			string newString = "";
+
+			for(unsigned int i = 0; i < argList.size(); i++)
+			{
+				if( i != 0)
+				{ newString += " "; }
+
+				newString += argList[i];
+			}
+			return const_cast<char*>(newString.c_str());
 		}
 
 		string getCommand_c_str()
 		{
-			string commandStr = "";
-			return commandStr;
+			string commandString = "";
+			return commandString;
 		}
 
 		friend ostream& operator <<(ostream& os, const Command c);
-};
+	};
 
-ostream& operator <<(ostream& os, const Command c)
-{
-	if(c.getArgListStr().length() > 0)
+	ostream& operator <<(ostream& os, const Command c)
 	{
-		os << c.getExec() << " " << c.getArgListStr();
+		if(c.getArgListStr().length() > 0)
+		{ os << c.getExec() << " " << c.getArgListStr(); }
+		else
+		{ os << c.getExec(); }
+	
+		return os;
 	}
-	else
-	{ os << c.getExec(); }
-
-	return os;
-}
 
 #endif
-
