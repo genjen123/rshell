736c76a6cdb15637c04f6cb6d7106192956b0508
Jenny Trang Nguyen <Jenny@MacBook-Pro.local>
2016-02-12 22:14:39 -0800

    finalized compilable

--- src/shell.h
+++ src/shell.h
@@ -1,11 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+//std included
 #include <iostream>
-#include <vector>
 #include <string>
-#include <unistd.h>
+#include <vector>
 #include <stdio.h>
+#include <utility>
+//syscalls
+#include <unistd.h> //execvp fork cd ..
+#include <sys/wait.h> //waitpid
+#include <errno.h>
+//files included
 #include "parse.h"
 #include "commandnode.h"
 using namespace std;
@@ -14,10 +20,11 @@ class Shell
 {
 	private:
 		bool status;		//check status of whether shell cont. running or not
-	
+  
 	public:
-		Shell(){};		//constructor
-		bool getStatus()
+		Shell(){};			//default constructor
+		
+		bool getStatus()	//get current status of shell
 		{ return status; }
 
 		void start()		//start shell program
@@ -25,129 +32,300 @@ class Shell
 
 		void finish()		//shell is done
 		{ status = false; }
-		
-		bool execute(const CommandNode &cn)
+
+		pair<bool,bool> execute(const CommandNode & cn)
 		{
+			//initialize variables and values
 			bool returnStatus = false;
-			cout << cn << endl;
-			return returnStatus;
+			bool child = false;
+			string exe = cn.getCommand().getExec();
+
+			//cout << "exe: " << exe << endl; 
+			//cout << cn << endl;
+
+			//Check to see if command is cd
+			if(exe == "cd")
+			{
+				//cout << "cd might happend:" << endl;
+				int argSize = cn.getCommand().getArgList().size();
+				
+				if(argSize > 0)
+				{
+					if(cn.getCommand().getArgListStr() == "~")
+					{ chdir(getenv("HOME")); }
+					else
+					{ chdir(cn.getCommand().getArgList_c_str()); }
+					//cout << "error:" << errno << endl;
+				}
+				else
+				{
+					//cd to home directory
+					chdir(getenv("HOME"));
+					//cout << "error:" << errno << endl;
+				}
+				
+				if(errno)
+				{
+					cout << "rshell: " << exe << ": " +  cn.getCommand().getArgListStr() << ": " << strerror(errno) << endl;
+					child = false;
+					return make_pair(false,child);
+				}
+				else
+				{
+					child = false;
+					return make_pair(true,child);
+				}
+			}
+			else
+			{
+				pid_t pid = fork();
+
+				if(pid > 0)
+				{
+					//I am the parent process
+					//add later check for background check
+					int status = errno;
+					waitpid(pid,&status,0);
+					//cout << "Parent end" << endl;
+					if(errno)
+					{
+						returnStatus = false;
+						child = false;
+					}
+					else
+					{
+						returnStatus = true;
+						child = false;
+					}
+				}
+				else if(pid == 0)
+				{
+					// I am the child process
+					//cout << "child start" << endl;
+					//execute command
+					//What about ~ in command?
+					string command = exe;
+					//construct arguement list c string array
+					//int size = 32;
+					char *arguementList[1024];		//standard amount
+					memset(arguementList,0,sizeof(arguementList));
+
+					//add command 
+					arguementList[0] =  const_cast<char*>(command.c_str());
+
+					for(unsigned int i = 0; i < cn.getCommand().getArgList().size(); i++)
+					{
+						//add c string to arguement list
+						//need to use const cast to remove error: invalid conversion from ‘const char*’ to ‘char*’ [-fpermissive] error
+						arguementList[i+1] = const_cast<char*>(cn.getCommand().getArgList().at(i).c_str());
+						//cout << arguementList[i] << endl;
+						arguementList[i+2] = 0;
+					}
+
+					//cout << command.c_str() << " ";
+					//for(unsigned int i = 0; i <  cn.getCommand().getArgList().size() && i < 33 ; i++)
+					//{ cout << arguementList[i] << " "; }
+					//cout << endl;
+					//cout << "execvp" << endl;
+
+					int status = 0; 
+					status = execvp(command.c_str(),arguementList);
+
+					//cout << "status::"<< status << endl;
+					//cout << "execvp end" << endl;
+					if(status >= 0)
+					{
+						returnStatus = true;
+						child = true;
+					}
+					else
+					{
+						cout << "rshell: " << exe << ": " +  cn.getCommand().getArgListStr() << ": " << strerror(errno) << endl;
+						returnStatus = false;
+						child = true;
+					}
+				}
+				else
+				{
+					//an error occurred
+					cerr << "Fork failed" << endl;
+					returnStatus = false;
+					child = true;
+				}
+			}
+			//cout << "child end" << endl;
+			//check status
+			return make_pair(returnStatus,child);
 		}
 
-		void initialize()	//start shell
+		//start shell
+		void initialize()	
 		{
-			start();
+			start();	
+			bool child = false;
 
-			while(status)	//user input and parse commands
+			//display current logged in user & hostname then start shell program
+			while(status && !child)		
 			{
-				//initializing variables
-				string input, command;
-				Parse str;				
-		
-				//generate username (extra credit)
-				char username[1024] = {0};
-				int getUsername_resp = getlogin_r(username, sizeof(username) - 1);
+				string input; 
 
-				char hostName[1024] = {0};
-				int getHostName_resp = gethostname(hostName, sizeof(hostName) - 1);
+				//Output prompt
+				char username[1024] ={0};
+				int getUsername_resp = getlogin_r(username,sizeof(username)-1);
 
-				if((getHostName_resp == 0) && (getUsername_resp == 0))
-				{ printf("%s@%s", username, hostName); }
+				/*
+				//printf("%s\n",username);
+				char* home = getlogin();
+				if(home != NULL)
+				printf("%s\n",home);
+				*/
+				char hostname [1024] = {0};
+				int getHostname_resp = gethostname(hostname,sizeof(hostname)-1);
+				//cout << getUsername_resp << " " << getHostname_resp<< endl;
 
-				cout << "$ ";
+				if((getHostname_resp == 0) && (getUsername_resp == 0))
+				{
+					//string username_str = "";
+					//string hostname_str = "";
+					//cout << username_str <<"@"<<hostname_str;
+					printf("%s@%s", username,hostname);
+				}
+				cout <<"$ ";	//start getting inputs for shell program
 
 				getline(cin, input);	//get line input
+
+				//Parse input
+				Parse p;				//parse object
+				input = p.ltrimr(input," ");
 				//cout << input << endl;
-	
+				
 				if(input != "exit")
 				{
-					string parseTag = str.split(input, "#").at(0);	//parse #
-
-					vector<string> parseCom = str.split(parseTag, ";");	//parse ;
+					//Split inputs on # and ignore everything afterwards
+					string commands = p.split(input,"#")[0];
 					
-					//go through command parts and create list of commandNodes
+					//Split on ';'
+					vector<string> commandParts = p.split(commands,";");
+					
+					//Now go through command parts and create List of commandNode
+					string command = "";
 					vector<CommandNode> commandList;
-					for(unsigned int i = 0; i < parseCom.size(); i++)
+
+					for(unsigned int i = 0; i < commandParts.size(); i++)
 					{
-						command = parseCom.at(i);	//separate commands
+						//cout << commandParts[i] << endl;
+						//Parse now on || and &&
+						command = commandParts[i];
+						string tmp = "";
+						int orIndex = command.find("||");
+						int andIndex = command.find("&&");
+						string connector ="";
 
-						//initializing variables
-						string temp = "";
-						string connector = "";
-
-						//setting positions for || and &&
-						int orCon = command.find("||");
-						int andCon = command.find("&&");
-
-						//if || or && is found
-						while((orCon != string::npos) || (andCon != string::npos))
+						while( (int(orIndex) != int(string::npos)) or (int(andIndex) != int(string::npos)) )
 						{
-							int first = 0;	//position temp variable
+							//cout << command << endl;
+							int first = 0;
 
-							if((orCon != string::npos) && (andCon != string::npos))		//if both are found
+							if( (int(orIndex) != int(string::npos)) && (int(andIndex) != int(string::npos)) )
 							{
-								//orCon < andCon then first = orCon else first = andCon
-								if(orCon < andCon)
+								//first = (orIndex < andIndex) ? orIndex : andIndex;
+								if(orIndex < andIndex)
 								{
-									first = orCon;
+									first = orIndex;
 									connector = "||";
 								}
-								else if(orCon > andCon)
+								else if(orIndex > andIndex)
 								{
-									first = andCon;
+									first = andIndex;
 									connector = "&&";
 								}
-							}
-							else if((orCon != string::npos) && (andCon == string::npos))	//if only || is found
-							{ 
-								first = orCon; 
+		            		}
+							else if( (int(orIndex) != int(string::npos)) && (int(andIndex) == int(string::npos)) )
+							{
+								first = orIndex;
 								connector = "||";
 							}
-							else if((orCon == string::npos) && (andCon != string::npos))	//if only && is found
-							{ 
-								first = andCon; 
+							else if ( (int(orIndex) == int(string::npos)) && (int(andIndex) != int(string::npos)) )
+							{
+								first = andIndex;
 								connector = "&&";
 							}
-
-							temp = command.substr(0, first);	//get parsed string
-							commandList.push_back(CommandNode(temp, connector));
-							command = command.substr(first + connector.length());
-							orCon = command.find("||");			//find repeat ||
-							andCon = command.find("&&");		//find repeat &&
+							
+							tmp = command.substr(0,first);
+							commandList.push_back(CommandNode(tmp,connector));
+							command = command.substr(first+connector.length());
+							orIndex = command.find("||");
+							andIndex = command.find("&&");
 						}
 
-						if(command.length() > 0)				//if string isn't empty
-						{ commandList.push_back(CommandNode(command, ";")); }
+						if(command.length() > 0)
+						{ commandList.push_back(CommandNode(command,";")); }
+					}
+					//cout << commandList.size() << endl;
+					//Now for through each command in commandList and exectue them if possible
+					//https://en.wikipedia.org/wiki/Bash_(Unix_shell)
 
-						//go through each command and execute them
-						bool prevReturnStat = false;
-						bool currentReturnStat = false;
+					bool chainStatus = true;
+					Connector preCon;
+					bool currentReturnStatus = false;
 
-						for(int j = 0; j < commandList.size(); j++)
+					for(unsigned int i = 0; i < commandList.size(); i++)
+					{
+						//Execute command node
+						CommandNode currentNode = commandList[i];
+						Connector con = currentNode.getConnector();
+						bool run = false;
+
+						//skip logic
+						if(chainStatus)
+						{ run = true; }
+						else
 						{
-							//cout << i + 1 << ") [" << j + 1 << "/" << parseCon.size()
-							//	 << "]" << str.leftTrim(parseCon.at(j), " ") << endl;
-							
-							currentReturnStat = execute(commandList.at(i));
-							prevReturnStat = currentReturnStat;
-							
-							if(!prevReturnStat)
+							if(con.isOR())
 							{
-								//do something here
+								//cout << "has ||" << endl;
+								run = true;
+								chainStatus = true;
 							}
-							
-						}	
+							else if(con.isAND())
+							{
+								//cout << "has &&" << endl;
+								run = false;
+							}
+						}
+						
+						if(con.isSEMI())
+						{
+							//cout << "has ;" << endl;
+							chainStatus = true;
+						}
+
+						if(run)
+						{
+							pair<bool,bool> ret;
+							ret = execute(commandList[i]);
+							currentReturnStatus = ret.first;
+							child = ret.second;
+
+							//assign new status
+							chainStatus = currentReturnStatus && chainStatus;
+						}
+						else
+						{ /*cout << "no run" << endl;*/ }
+
+						//assign new status
+						//chainStatus = currentReturnStatus && chainStatus;
+						preCon = con;
+
+						if(child)
+						{ break; }
 					}
 				}
 				else
 				{ break; }	//if user input is exit then break loop and exit
 			}
-			
 			finish();
 		}
 };
 
-
-
-
 #endif
-
