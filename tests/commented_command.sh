2e369c268b7840690a2119d5ce8a0ee1d646171d
Jenny Trang Nguyen <Jenny@MacBook-Pro.local>
2016-03-02 00:31:30 -0800

    simplified and added more test cases

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -2,4 +2,17 @@
 
 #tests commands with comments
 make all
-echo -e "#do not print\necho output\n#exit\nexit\n" | bin/rshell
+echo -e "#do not print\nexit\n" | bin/rshell
+echo
+echo -e "echo a#hello\nexit\n" | bin/rshell
+echo
+echo -e "#exit\nexit\n" | bin/rshell
+echo
+echo -e "echo a #;\nexit\n" | bin/rshell
+echo
+echo -e "ls #-a\nexit\n" | bin/rshell
+echo
+echo -e " \nexit\n" | bin/rshell
+echo
+echo -e "\t\nexit\n" | bin/rshell 
+echo
--- tests/exit.sh
+++ tests/exit.sh
@@ -2,5 +2,17 @@
 
 #tests exit and commands with exit
 make all
-echo -e "cd .. || exit\n#exit\nexit\n" | bin/rshell
-
+echo -e "cd .. || exit\nexit\n" | bin/
+echo 
+echo -e "echo A#exit\nexit\n" | bin/rshell
+echo
+echo -e "exit\n" | bin/rshell
+echo
+echo -e "ls && exit\n" | bin/rshell
+echo
+echo -e "echo A && exit\n" | bin/rshell
+echo
+echo -e "echo A || exit\n" | bin/rshell
+echo
+echo -e "echo A; exit\n" | bin/rshell
+echo
